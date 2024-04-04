e9d8ef3e644022b390a0c53195cdfbdeef79681f
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2024-04-04 18:07:10 -0300

    usando comando GET, com funcionalidades do type e click

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,18 @@
-/// <reference types="Cypress" />
+/// <reference types="Cypress" /> ///
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function(){
         cy.visit('./src/index.html')
+    })
 
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
-  
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function(){
+        cy.get('#firstName').click().type('Deivid').should('have.value', 'Deivid')
+        cy.get('#lastName').click().type('Fuzari').should('have.value', 'Fuzari')
+        cy.get('#email').click().type('deivid_fuzari@hotmail.com').should('have.value', 'deivid_fuzari@hotmail.com')
     })
   })
   
